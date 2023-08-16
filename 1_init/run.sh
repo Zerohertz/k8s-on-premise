3b13d8c90f737830794c2e0cc30c741e5ed60718
Zerohertz <ohg3417@gmail.com>
2023-08-16 19:39:21 +0900

    :rocket: Add: Init K8s

---
+++ 1_init/run.sh
@@ -0,0 +1,34 @@
+# ---------------------------- METRICS-SERVER ---------------------------- #
+kubectl apply -f metrics-server.yaml
+
+# ---------------------------- LOCAL-PATH ---------------------------- #
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.24/deploy/local-path-storage.yaml
+kubectl apply -f local-path.yaml
+
+# ---------------------------- METALLB ---------------------------- #
+# https://metallb.universe.tf/installation/
+kubectl delete ns metallb-system
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.10/config/manifests/metallb-native.yaml
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+
+echo "IS 'metallb-system' Done?"
+read GO
+
+kubectl apply -f metallb.yaml
+
+# ---------------------------- TRAEFIK ---------------------------- #
+# helm show values traefik/traefik > traefik.yaml
+# helm repo add traefik https://helm.traefik.io/traefik
+# helm repo update
+helm uninstall traefik -n kube-system
+helm install traefik traefik/traefik -f traefik-values.yaml -n kube-system
+kubectl apply -f traefik-oauth.yaml
+# helm uninstall traefik -n kube-system
+
+# ---------------------------- NGINX ---------------------------- #
+# helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+# helm repo update
+# kubectl create ns nginx
+# helm install my-nginx ingress-nginx/ingress-nginx -n nginx
