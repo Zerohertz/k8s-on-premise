7a69c9694cb1e329183ab6b59e5bda2a6a22b5e7
Zerohertz <ohg3417@gmail.com>
2023-08-16 19:44:16 +0900

    :tada: Add: Apache Airflow

---
+++ airflow/run.sh
@@ -0,0 +1,10 @@
+# ---------------------------- AIRFLOW ---------------------------- #
+kubectl delete pv airflow-local-dags-pv
+kubectl delete pv airflow-local-logs-pv
+kubectl create ns airflow
+kubectl apply -f storage.yaml
+
+helm upgrade --install airflow apache-airflow/airflow --namespace airflow -f values.yaml --version 1.10.0 --debug
+
+# ---------------------------- TRAEFIK ---------------------------- #
+kubectl apply -f traefik.yaml
