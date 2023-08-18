eb07203b3b10445783caba21e8aabb40bd557d08
Zerohertz <ohg3417@gmail.com>
2023-08-18 16:12:35 +0900

    :recycle: Refactor: Format

--- airflow/run.sh
+++ airflow/run.sh
@@ -4,7 +4,7 @@ kubectl delete pv airflow-local-logs-pv
 kubectl create namespace airflow
 kubectl apply -f storage.yaml
 
-helm upgrade --install airflow apache-airflow/airflow --namespace airflow -f values.yaml --version 1.10.0 --debug
+helm upgrade --install airflow apache-airflow/airflow -f values.yaml --namespace airflow --version 1.10.0 --debug
 
 # ---------------------------- TRAEFIK ---------------------------- #
 kubectl apply -f traefik.yaml
