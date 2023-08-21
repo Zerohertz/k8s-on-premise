b84d2fe2c5e90ae3ea60fc9aa8c96b33ca460fa5
Zerohertz <ohg3417@gmail.com>
2023-08-21 16:38:13 +0900

    :memo: Refactor: argo-cd/run.sh

--- argo-cd/run.sh
+++ argo-cd/run.sh
@@ -1,11 +1,5 @@
 # ---------------------------- ARGOCD ---------------------------- #
-# helm repo add argo https://argoproj.github.io/argo-helm
-# helm repo update
-# helm show values argo/argo-cd --version 5.34.6 > values.yaml
-
-helm upgrade --install argo-cd argo/argo-cd -f values.yaml --namespace argo-cd --create-namespace --version 5.34.6 #--debug
-
-# helm uninstall argo-cd -n argo-cd
+helm upgrade --install argo-cd argo/argo-cd -f values.yaml --namespace argo-cd --create-namespace --version 5.34.6
 
 # ---------------------------- TRAEFIK ---------------------------- #
 kubectl apply -f traefik.yaml
