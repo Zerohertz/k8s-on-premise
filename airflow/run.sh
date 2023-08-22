a5b5f0d39d6e9970c463467d2b8d1540b326d650
Zerohertz <ohg3417@gmail.com>
2023-08-22 12:58:25 +0900

    :sparkles: Feat: airflow-webserver-secret-key

--- airflow/run.sh
+++ airflow/run.sh
@@ -4,7 +4,8 @@ kubectl delete pv airflow-local-logs-pv
 kubectl create namespace airflow
 kubectl apply -f storage.yaml
 
-helm upgrade --install airflow apache-airflow/airflow -f values.yaml --namespace airflow --version 1.10.0 --debug
+kubectl create secret generic airflow-webserver-secret-key --from-literal="webserver-secret-key=$(python3 -c 'import secrets; print(secrets.token_hex(16))')" --namespace airflow
+helm upgrade --install airflow apache-airflow/airflow -f values.yaml --namespace airflow --version 1.10.0
 
 # ---------------------------- TRAEFIK ---------------------------- #
 kubectl apply -f traefik.yaml
