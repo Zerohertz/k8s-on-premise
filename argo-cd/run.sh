5df07fd8c739baa9cee3e246ac15195bafe8db2f
Zerohertz <ohg3417@gmail.com>
2024-08-04 18:06:21 +0900

    :fire: update: Argo CD

--- argo-cd/run.sh
+++ argo-cd/run.sh
@@ -1,5 +1,9 @@
 # ---------------------------- ARGOCD ---------------------------- #
-helm upgrade --install argo-cd argo/argo-cd -f values.yaml --namespace argo-cd --create-namespace --version 5.34.6
+# helm repo add argo https://argoproj.github.io/argo-helm
+# helm repo update
+# helm search repo argo/argo-cd --versions | head -n 5
+# helm show values argo/argo-cd --version 7.3.11 > argocd.yaml
+helm upgrade --install argo-cd argo/argo-cd -f values.yaml --namespace argo-cd --create-namespace --version 7.3.11
 
 # ---------------------------- TRAEFIK ---------------------------- #
 kubectl apply -f traefik.yaml
