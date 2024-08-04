b3257e2deed5b240849a6d72bfadb72b1ead4e36
Zerohertz <ohg3417@gmail.com>
2024-08-04 17:47:13 +0900

    :bento: update: default setup

--- k8s/run.sh
+++ k8s/run.sh
@@ -1,17 +1,17 @@
 # ---------------------------- METRICS-SERVER ---------------------------- #
+# kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 kubectl apply -f metrics-server.yaml
 
 # ---------------------------- LOCAL-PATH ---------------------------- #
-kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.24/deploy/local-path-storage.yaml
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.28/deploy/local-path-storage.yaml
 kubectl apply -f local-path.yaml
 
 # ---------------------------- METALLB ---------------------------- #
 # https://metallb.universe.tf/installation/
-kubectl delete namespace metallb-system
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.10/config/manifests/metallb-native.yaml
-kubectl get configmap kube-proxy -n kube-system -o yaml | \
-sed -e "s/strictARP: false/strictARP: true/" | \
-kubectl apply -f - -n kube-system
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.8/config/manifests/metallb-native.yaml
+kubectl get configmap kube-proxy -n kube-system -o yaml |
+	sed -e "s/strictARP: false/strictARP: true/" |
+	kubectl apply -f - -n kube-system
 
 echo "IS 'metallb-system' Done?"
 read GO
@@ -19,12 +19,13 @@ read GO
 kubectl apply -f metallb.yaml
 
 # ---------------------------- TRAEFIK ---------------------------- #
-# helm show values traefik/traefik > traefik.yaml
 # helm repo add traefik https://helm.traefik.io/traefik
 # helm repo update
-helm uninstall traefik -n kube-system
-helm install traefik traefik/traefik -f traefik-values.yaml -n kube-system
+# helm search repo traefik/traefik --versions | head -n 5
+# helm show values traefik/traefik --version 30.0.2 >traefik.yaml
 # helm uninstall traefik -n kube-system
+
+helm install traefik traefik/traefik -f traefik-values.yaml -n kube-system --version 30.0.2
 
 # ---------------------------- OAUTH ---------------------------- #
 kubectl create namespace oauth
